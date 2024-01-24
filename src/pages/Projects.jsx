2f6040686a9fadc90333839c3ef3eef01c8f3873
miguel <miguelbranagan30@gmail.com>
2024-01-23 21:00:56 -0400

    adding scroll to top feature

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import PropTypes from "prop-types";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
@@ -82,23 +83,29 @@ ProjectCard.propTypes = {
 export default function Projects() {
   const { t } = useTranslation();
 
+  useEffect(() => {
+    window.scrollTo(0, 0); // Scroll up when the page load
+  }, []);
+
   return (
     <section className="max-w-2xl mx-auto mt-20 mb-14 lg:max-w-3xl xl:max-w-6xl">
       <Link to="/">
-      <div className="flex items-center gap-2">
-        <svg
-          className="w-2 stroke-black dark:stroke-white"
-          viewBox="0 0 5 8"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M4.5 1L1.5 4L4.5 7"
-            stroke="current"
-          />
-        </svg>
-        <span className="font-semibold font-SourceSansPro font dark:text-white">{t("homeProjectButtonm")}</span>
-      </div>
+        <div className="flex items-center gap-2">
+          <svg
+            className="w-2 stroke-black dark:stroke-white"
+            viewBox="0 0 5 8"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M4.5 1L1.5 4L4.5 7"
+              stroke="current"
+            />
+          </svg>
+          <span className="font-semibold font-SourceSansPro font dark:text-white">
+            {t("homeProjectButtonm")}
+          </span>
+        </div>
       </Link>
 
       <h1 className="mt-6">{t("projectsTitle")}</h1>
