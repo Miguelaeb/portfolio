271a07a0ad8c655876319e012e048cbae66a8016
miguel <miguelbranagan30@gmail.com>
2024-01-30 13:39:18 -0400

    form submission working

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,118 +1,116 @@
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { useForm, ValidationError } from "@formspree/react";
 import { useTranslation } from "react-i18next";
 
 export default function Contact() {
   const { t } = useTranslation();
+  const [state, handleSubmit] = useForm("mzbndqzn");
 
-  const validate = (values) => {
-    const errors = {};
-
-    if (!values.name) {
-      errors.name = "Name is required";
-    }
-
-    if (!values.email) {
-      errors.email = "Email is required";
-    } else if (!/\S+@\S+\.\S+/.test(values.email)) {
-      errors.email = "Email is invalid";
-    }
-
-    if (!values.message) {
-      errors.message = "Message is required";
-    }
-
-    return errors;
-  };
-
-  const handleSubmit = (values, { setSubmitting }) => {
-    // Perform form submission logic here
-    console.log("Form submitted successfully!", values);
-    setSubmitting(false);
-  };
+  if (state.succeeded) {
+    return (
+      <p className="mt-20">
+        Thanks for your message, we will reply as soon as possible!
+      </p>
+    );
+  }
 
   return (
     <section className="mt-20">
       <h2>{t("contactTitle")}</h2>
       <div className="line w-full h-[2px] bg-primaryGrey mt-2"></div>
 
-      <div className="mt-14">
-        <div className="mt-10 lg:mt-14 xl:mt-0">
-          <Formik
-            initialValues={{ name: "", email: "", message: "" }}
-            validate={validate}
-            onSubmit={handleSubmit}
-          >
-            <Form>
-              <div>
-                <label
-                  className="block text-lg font-semibold text-left font-SourceSansPro dark:text-white"
-                  htmlFor="name"
-                >
-                  {t("contactNameLabel")}
-                </label>
-                <Field
-                  className="w-full p-3 border-2 border-solid rounded-lg shadow-sm font-SourceSansPro focus:font-SourceSansPro focus:outline-none dark:text-white bg-primaryGrey/10 backdrop:blur-sm"
-                  type="text"
-                  id="name"
-                  name="name"
-                />
-                <ErrorMessage
-                  className="text-left dark:text-white"
-                  name="name"
-                  component="div"
-                />
-              </div>
+      <form
+        className="mt-10 lg:mt-14"
+        onSubmit={handleSubmit}
+      >
+        <div className="flex flex-col gap-4">
+          <div>
+            <label
+              className="block text-lg font-semibold text-left font-SourceSansPro dark:text-white"
+              htmlFor="firstName"
+            >
+              {t("contactNameLabel")}
+            </label>
+            <input
+              className="w-full p-3 border-2 border-solid rounded-lg shadow-sm font-SourceSansPro focus:font-SourceSansPro focus:outline-none dark:text-white bg-primaryGrey/10 backdrop:blur-sm"
+              id="firstName"
+              type="text"
+              name="firstName"
+            />
+            <ValidationError
+              prefix="firstName"
+              field="firstName"
+              errors={state.errors}
+            />
+          </div>
 
-              <div>
-                <label
-                  className="block mt-4 text-lg font-semibold text-left font-SourceSansPro dark:text-white"
-                  htmlFor="email"
-                >
-                  {t("contactEmailLabel")}
-                </label>
-                <Field
-                  className="w-full p-3 border-2 border-solid rounded-lg shadow-sm font-SourceSansPro focus:font-SourceSansPro focus:outline-none dark:text-white bg-primaryGrey/10 backdrop:blur-sm"
-                  type="email"
-                  id="email"
-                  name="email"
-                />
-                <ErrorMessage
-                  className="text-left dark:text-white"
-                  name="email"
-                  component="div"
-                />
-              </div>
+          <div>
+            <label
+              className="block text-lg font-semibold text-left font-SourceSansPro dark:text-white"
+              htmlFor="lastName"
+            >
+              {t("contactLastNameLabel")}
+            </label>
+            <input
+              className="w-full p-3 border-2 border-solid rounded-lg shadow-sm font-SourceSansPro focus:font-SourceSansPro focus:outline-none dark:text-white bg-primaryGrey/10 backdrop:blur-sm"
+              id="lastName"
+              type="text"
+              name="lastName"
+            />
+            <ValidationError
+              prefix="lastName"
+              field="lastName"
+              errors={state.errors}
+            />
+          </div>
 
-              <div>
-                <label
-                  className="block mt-4 text-lg font-semibold text-left font-SourceSansPro dark:text-white"
-                  htmlFor="message"
-                >
-                  {t("contactMessageLabel")}
-                </label>
-                <Field
-                  className="w-full p-3 border-2 border-solid rounded-lg shadow-sm resize-none font-SourceSansPro focus:font-SourceSansPro focus:outline-none bg-portfolio-primary-color-grey/10 backdrop:blur-sm dark:text-white bg-primaryGrey/10"
-                  as="textarea"
-                  id="message"
-                  name="message"
-                />
-                <ErrorMessage
-                  className="text-left dark:text-white"
-                  name="message"
-                  component="div"
-                />
-              </div>
+          <div>
+            <label
+              className="block text-lg font-semibold text-left font-SourceSansPro dark:text-white"
+              htmlFor="email"
+            >
+              {t("contactEmailLabel")}
+            </label>
+            <input
+              className="w-full p-3 border-2 border-solid rounded-lg shadow-sm font-SourceSansPro focus:font-SourceSansPro focus:outline-none dark:text-white bg-primaryGrey/10 backdrop:blur-sm"
+              id="email"
+              type="email"
+              name="email"
+            />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
+          </div>
 
-              <button
-                className="block px-10 py-2 mt-6 text-lg font-semibold transition duration-300 ease-in-out rounded-md font-SourceSansPro text-background-color bg-primaryRed lg:px-20 hover:scale-105 dark:bg-[#3B82F6]"
-                type="submit"
-              >
-                {t("submitButton")}
-              </button>
-            </Form>
-          </Formik>
+          <div>
+            <label
+              className="block mt-4 text-lg font-semibold text-left font-SourceSansPro dark:text-white"
+              htmlFor="message"
+            >
+              {t("contactMessageLabel")}
+            </label>
+
+            <textarea
+              className="w-full h-[200px] p-3 border-2 border-solid rounded-lg shadow-sm resize-none font-SourceSansPro focus:font-SourceSansPro focus:outline-none bg-portfolio-primary-color-grey/10 backdrop:blur-sm dark:text-white bg-primaryGrey/10"
+              id="message"
+              name="message"
+            />
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={state.errors}
+            />
+          </div>
         </div>
-      </div>
+        <button
+          className="block px-10 py-2 mt-6 text-lg font-semibold transition duration-300 ease-in-out rounded-md font-SourceSansPro text-background-color bg-primaryRed lg:px-20 hover:scale-105 dark:bg-[#3B82F6]"
+          type="submit"
+          disabled={state.submitting}
+        >
+          Submit
+        </button>
+      </form>
     </section>
   );
 }
